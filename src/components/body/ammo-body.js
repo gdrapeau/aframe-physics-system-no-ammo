2e2c8c06bd3549cf2c682c0e895482ac58ac1f36
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-10-11 14:43:00 +0100

    Try fix suggested in #47

--- src/components/body/ammo-body.js
+++ src/components/body/ammo-body.js
@@ -365,7 +365,8 @@ let AmmoBody = {
     if (this.localScaling) Ammo.destroy(this.localScaling);
     if (this.compoundShape) Ammo.destroy(this.compoundShape);
     if (this.body) {
-      Ammo.destroy(this.body);
+      this.system.driver.removeBody(this.body)
+      //Ammo.destroy(this.body);
       delete this.body;
     }
     Ammo.destroy(this.rbInfo);
